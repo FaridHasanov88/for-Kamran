93736ea10f62f1cd6946b4c5c77481258097b536
FaridHasanov88 <hasanov.farid88@gmail.com>
2022-03-27 19:36:41 +0400

    Initial version

---
+++ bar.js
@@ -0,0 +1,163 @@
+function GoP11() {
+    const P1 = document.getElementById("Player1In");
+    let w1= P1.clientWidth
+    if (w1>=1500){
+        console.log("break")
+    }
+    else {
+        P1.style.width = w1 + 20 + 'px';
+    }
+}
+
+function GoP15() {
+    const P1 = document.getElementById("Player1In");
+   // const P2 = document.getElementById("img1");
+    let w1= P1.clientWidth
+    //let w2=P2.style.left
+    
+    if (w1>=1500){
+        console.log("break")
+    }
+    else {
+        P1.style.width = w1 + 100 + 'px';
+        //P2.style.left = w2 + 100 + 'px';
+
+    }
+}
+function GoP110() {
+    const P1 = document.getElementById("Player1In");
+    let w1= P1.clientWidth
+    if (w1>=1500){
+        console.log("break")
+    }
+    else {
+        P1.style.width = w1 + 200 + 'px';
+    }
+}
+
+function GoP21() {
+    const P2 = document.getElementById("Player2In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 20 + 'px';
+    }
+}
+
+function GoP25() {
+    const P2 = document.getElementById("Player2In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 100 + 'px';
+    }
+}
+function GoP210() {
+    const P2 = document.getElementById("Player2In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 200 + 'px';
+    }
+}
+
+function GoP31() {
+    const P2 = document.getElementById("Player3In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 20 + 'px';
+    }
+}
+
+function GoP35() {
+    const P2 = document.getElementById("Player3In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 100 + 'px';
+    }
+}
+function GoP310() {
+    const P2 = document.getElementById("Player3In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 200 + 'px';
+    }
+}
+function GoP41() {
+    const P2 = document.getElementById("Player4In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 20 + 'px';
+    }
+}
+
+function GoP45() {
+    const P2 = document.getElementById("Player4In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 100 + 'px';
+    }
+}
+function GoP410() {
+    const P2 = document.getElementById("Player4In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 200 + 'px';
+    }
+}
+
+function GoP51() {
+    const P2 = document.getElementById("Player5In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 20 + 'px';
+    }
+}
+
+function GoP55() {
+    const P2 = document.getElementById("Player5In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 100 + 'px';
+    }
+}
+function GoP510() {
+    const P2 = document.getElementById("Player5In");
+    let w2= P2.clientWidth
+    if (w2>=1500){
+        console.log("break")
+    }
+    else {
+        P2.style.width = w2 + 200 + 'px';
+    }
+}
